5da39fe53f06a66ed7c23ca068dec51752f06214
jmounteer <mounteerjaden@gmail.com>
2021-09-16 07:13:44 -0600

    removed daddy controller

--- js/developerDaddyController.js
+++ js/developerDaddyController.js
@@ -1,14 +1,11 @@
 
 /* Fetch the new message from the file using AJAX */
 function getNewMessage(textFile) {
-    console.log("Inside the getNewMessage function");
     const xhttp = new XMLHttpRequest();
 
     xhttp.onload = function() {
-        console.log("Loading AJAX request");
         // Split the contents of the text file into an array
         let responseArray = this.responseText.split("\n");
-        console.log("The response array is: " + responseArray);
 
         // Find a random number based on the array's length
         var randomNumber = Math.floor(Math.random() * responseArray.length);
